a38b5f5a8dd9549008eb7f0a7fad22431950a4f7
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-01-10 12:38:58 +0530

    Remove legacy code

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-/*
-function hash() {
-	document.getElementById("result").innerText = "hashing . . . "
-	stringg = document.getElementById("tbh").value
-	fetch("https://sha256.chiroyce.repl.co/hash?string="+stringg)
- 	 .then((res) => res.json())
-  	 .then((res) => {
-		document.getElementById("result").innerText = `${res.hash}`
- 	 });
-}
-*/
-
-// haha more readable code go brr..
-
-let btn = document.getElementById("btn");
 let hash = document.getElementById("result");
+let btn = document.getElementById("btn");
 let string = document.getElementById("tbh");
 
 btn.onclick = async (e) => {
 	let res = await fetch(`https://sha256.chiroyce.repl.co/hash?string=${string.value}`);
 	res = await res.json();
 	hash.innerText = res.hash;
-	
 }
