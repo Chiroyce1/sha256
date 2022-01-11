c1cad7716152c805353bdf01946c2294403edcc9
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-01-11 09:22:37 +0530

    fix indentation

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let string = document.getElementById("tbh");
 const encoder = new TextEncoder();
 
 btn.onclick = async (e) => {
-	// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
-	const digest = await window.crypto.subtle.digest('SHA-256', encoder.encode(string.value));
-	const hashArray = Array.from(new Uint8Array(digest));
+    // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
+    const digest = await window.crypto.subtle.digest('SHA-256', encoder.encode(string.value));
+    const hashArray = Array.from(new Uint8Array(digest));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-	hash.innerText = hashHex;
+    hash.innerText = hashHex;
 }
