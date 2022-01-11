b8be287b89b2bf4faa44e7cfb323645d6258c42c
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-11 16:37:27 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let hash = document.getElementById("result");
 let btn = document.getElementById("btn");
 let string = document.getElementById("tbh");
+const encoder = new TextEncoder();
 
 btn.onclick = async (e) => {
-	const digest = await window.crypto.subtle.digest('SHA-256', string.value);
+	const digest = await window.crypto.subtle.digest('SHA-256', encoder.encode(string.value));
 	hash.innerText = digest;
 }
