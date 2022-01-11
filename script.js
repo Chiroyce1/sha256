5c7fd185553b90b73f0ce96a9abd4a66e051f55d
webdev03 <75148774+webdev03@users.noreply.github.com>
2022-01-11 16:39:46 +1300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ let string = document.getElementById("tbh");
 const encoder = new TextEncoder();
 
 btn.onclick = async (e) => {
+	// https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
 	const digest = await window.crypto.subtle.digest('SHA-256', encoder.encode(string.value));
-	hash.innerText = digest;
+	const hashArray = Array.from(new Uint8Array(hashBuffer));
+        const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+	hash.innerText = hashHex;
 }
